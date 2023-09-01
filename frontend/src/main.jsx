dead2fdadcad3e8ecd36c2c4fffa43fb0526da8c
Naveen <naveen1808@live.in>
2023-09-01 18:59:38 +0530

    added private route where shipping page works on login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,6 +18,7 @@ import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
 import ShippingScreen from "./screens/ShippingScreen.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,7 +28,11 @@ const router = createBrowserRouter(
       <Route path={"/cart"} element={<CartScreen />} />
       <Route path={"/login"} element={<LoginScreen />} />
       <Route path={"/register"} element={<RegisterScreen />} />
-      <Route path={"/shipping"} element={<ShippingScreen />} />
+
+      {/*any private routes goes here*/}
+      <Route path={""} element={<PrivateRoute />}>
+        <Route path={"/shipping"} element={<ShippingScreen />} />
+      </Route>
     </Route>,
   ),
 );
