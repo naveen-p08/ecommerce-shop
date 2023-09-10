18c8e696c4cb66a8533523ad73ee3acf2bbe4c90
Naveen <naveen1808@live.in>
2023-09-10 18:01:13 +0530

    modified mainscreen to get products with pagenumber

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,12 +1,15 @@
 import {Row, Col} from "react-bootstrap";
+import {useParams} from "react-router-dom";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productApiSlice.js";
 import Loader from "../components/Loader.jsx";
 import Message from "../components/Message.jsx";
 
 function HomeScreen() {
-  const { data: products, isLoading, isError } = useGetProductsQuery();
-
+  const { pageNumber } = useParams();
+  const { data, isLoading, isError } = useGetProductsQuery({pageNumber});
+  
+  
   return (
     <>
       {isLoading ? (
@@ -15,10 +18,9 @@ function HomeScreen() {
         <Message variant={'danger'}>{isError?.error?.message}</Message>
       ) : (
         <>
-          {" "}
           <h1>Latest Products</h1>
           <Row>
-            {products.map((product) => (
+            {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
